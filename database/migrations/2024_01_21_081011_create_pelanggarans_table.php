66fa0d901f56a770439036a6dca1efe84ee7e5a7
luluhsnl <luthfiyahlulu2006@gmail.com>
2024-01-21 21:55:17 +0700

    first commit

---
+++ database/migrations/2024_01_21_081011_create_pelanggarans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pelanggarans', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('tanggal');
+            $table->enum('jenis_pelanggaran', ['kerajinan', 'kerapihan', 'sikap_perilaku']);
+            $table->string('nama_pelanggaran');
+            $table->integer('point');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelanggarans');
+    }
+};
